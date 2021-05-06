91bbb46a53b80124ec90b9c0bfd877fd97c37fca
Ignat <ignativanov1996@mail.ru>
2021-05-06 17:57:52 +0300

    2021.05.06.1757
    Stage 2 complete.

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -13,3 +13,25 @@ while True:
         print('Current data length is {}, {} symbols left'.format(len(data), 100 - len(data)))
 
 print('Final data string:', data, sep='\n')
+print()
+
+zeros = dict()
+ones = dict()
+
+for pointer in range(0, len(data) - 3):
+    if data[pointer + 3] == '0':
+        triad = data[pointer] + data[pointer + 1] + data[pointer + 2]
+        zeros.setdefault(triad, 0)
+        zeros[triad] += 1
+    elif data[pointer + 3] == '1':
+        triad = data[pointer] + data[pointer + 1] + data[pointer + 2]
+        ones.setdefault(triad, 0)
+        ones[triad] += 1
+
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            triad = str(x) + str(y) + str(z)
+            zeros.setdefault(triad, 0)
+            ones.setdefault(triad, 0)
+            print('{}{}{}: {},{}'.format(x, y, z, zeros.get(triad), ones.get(triad)))
