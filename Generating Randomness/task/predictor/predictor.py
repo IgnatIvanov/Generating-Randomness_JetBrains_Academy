16eaa5c163616f889f92c343fed4d06fa58f7d72
Ignat <ignativanov1996@mail.ru>
2021-05-06 17:31:55 +0300

    2021.05.06.1731
    Stage 1 complete.

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,1 +1,15 @@
-print('Hello, World!')
+# print('Hello, World!')
+data = ''
+while True:
+    print('Print a random string containing 0 or 1:', sep='\n')
+    user_in = str(input())
+    for digit in user_in:
+        if digit == '0' or digit == '1':
+            data += digit
+
+    if len(data) > 99:
+        break
+    else:
+        print('Current data length is {}, {} symbols left'.format(len(data), 100 - len(data)))
+
+print('Final data string:', data, sep='\n')
